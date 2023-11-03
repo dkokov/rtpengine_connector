a35ade4703ffd504dbc7b93fb1677c0939317a57
Dimitar Kokov <dimitar75@gmail.com>
2023-11-03 13:13:41 +0200

    edit cli/main.c,add arguments

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,18 +1,35 @@
 #include "../rtpengc.h"
 
-int main(void)
+#define UDP_SERVER_DEFAULT "127.0.0.1"
+#define UDP_PORT_DEFAULT 2223
+
+int main(int argc, char *argv[])
 {
 	int ret;
+	char ip[16];
+	short port = UDP_PORT_DEFAULT;
 
-	rtpengc_conf_t *cfg = rtpengc_init("udp","127.0.0.1",2223);
+	if(argc < 2) {
+		strcpy(ip,UDP_SERVER_DEFAULT); 
+	} else if (argc < 3) {
+		strcpy(ip,argv[1]);
+	} else {
+		strcpy(ip,argv[1]);
+		port = (short)atoi(argv[2]);
+	}
+
+	printf("Connecting to %s:%d\n",ip,port);
+
+	rtpengc_conf_t *cfg = rtpengc_init("udp",ip,port);
 
 	if(cfg != NULL) {
 	    ret = rtpengc_conn(cfg);
 	    if(ret == -1) {
-		/* Error */
-		printf("/nERROR/n");
-		return -1;
+			/* Error */
+			printf("/nERROR/n");
+			return -1;
 	    }
+
 	    rtpengc_ping(cfg);
 	    rtpengc_close(cfg);
 	}
